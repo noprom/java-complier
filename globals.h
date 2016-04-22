550a958b8b08e608cfae26e8898b41449b4379d7
noprom <tyee.noprom@qq.com>
2016-04-22 16:32:37 +0800

    add a global.h

---
+++ globals.h
@@ -0,0 +1,22 @@
+/****************************************************/
+/* File: globals.h                                  */
+/* 全局变量的声明,必须在其他文件之前引入                  */
+/****************************************************/
+#ifndef _GLOBALS_H_
+#define  _GLOBALS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+
+#endif
