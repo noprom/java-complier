8c49069fb15f3436b96abada439b9ba0e92f9a5c
noprom <tyee.noprom@qq.com>
2016-04-22 20:46:39 +0800

    add file description

--- globals.h
+++ globals.h
@@ -18,5 +18,10 @@
 #define TRUE 1
 #endif
 
+extern FILE* source;      /* 源代码文件 */
+extern FILE* listing;     /* 中间输出文件 */
+extern FILE* code;        /* 代码生成文件 */
+
+extern int lineno;        /* 输出的源代码文件行数 */
 
 #endif
