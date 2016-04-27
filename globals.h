fc4b4779e5db1f1cb7ce54fc3902697d13c07d34
noprom <tyee.noprom@qq.com>
2016-04-27 18:27:07 +0800

    add token type

--- globals.h
+++ globals.h
@@ -5,10 +5,11 @@
 #ifndef _GLOBALS_H_
 #define  _GLOBALS_H_
 
-#include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
+#include <vector>
+#include <string>
+#include <cstdio>
 
 #ifndef FALSE
 #define FALSE 0
@@ -21,11 +22,32 @@
 #define MAXRESERVED 8     /* MAXRESERVED = 保留字的数量 */
 
 typedef enum {
-  /* 特殊的token */
-  ENDFILE, ERROR,
-  /* 保留字 */
+    /* 
+     * 特殊的token
+     * 文件结束、错误的单词
+     */
+    ENDFILE, ERROR,
+    /**
+     * 注释
+     */
+    COMMENT,
+    /**
+     * 空格
+     */
+    SPACE,
+    /**
+     * 关键字
+     * abstract, boolean, break, byte, case, catch, char, class, const, continue,
+     * default, do, double, else, extends, false, final, finally, float, for, goto,
+     * if, implements, import, instanceof, int, interface, long, native, new, null,
+     * package, private, protected, public, return, short, static, super, switch,
+     * synchronized, this, throw, throws, transient, true, try, void, volatile, while
+     */
+    COMMENT,
 
-}
+    /* 保留字 */
+    
+} TokenType;
 
 extern FILE* source;      /* 源代码文件 */
 extern FILE* listing;     /* 中间输出文件 */
