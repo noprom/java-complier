7a8e880ed3c720070a3c31028225bbb7c8aae6ed
noprom <tyee.noprom@qq.com>
2016-04-27 14:05:25 +0800

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 //
-//  main.c
+//  main.cpp
 //  java_complier
 //
 //  Created by noprom on 4/27/16.
 //  Copyright © 2016 tyee.noprom@qq.com. All rights reserved.
 //
 
-#include <stdio.h>
+#include <iostream>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    printf("Hello, World!\n");
+    std::cout << "Hello, World!\n";
     return 0;
 }
