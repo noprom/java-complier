4744d95e0419d81c20ea56a1e51ef629e2c4dbcf
noprom <tyee.noprom@qq.com>
2016-04-27 18:49:15 +0800

    add assign operation

--- globals.h
+++ globals.h
@@ -24,28 +24,50 @@
 typedef enum {
     /* 
      * 特殊的token
-     * 文件结束、错误的单词
+     * 文件结束、
+     * 错误的单词: 0x100
      */
     ENDFILE, ERROR,
+    
     /**
-     * 注释
+     * 注释: 0x101
+     * 空格: 0x102
      */
-    COMMENT,
+    COMMENT, SPACE,
+    
     /**
-     * 空格
+     * 标志符: 0x104
+     * 数字,常量字符串
      */
-    SPACE,
+    ID, NUM, STR,
+
     /**
-     * 关键字
+     * 关键字: 0x103
+     * 布尔型: 0x105
+     * 字符型: 0x106
+     * 整型:   0x107
+     * 浮点型: 0x108
+     * 字符串: 0x109
+     *
      * abstract, boolean, break, byte, case, catch, char, class, const, continue,
      * default, do, double, else, extends, false, final, finally, float, for, goto,
      * if, implements, import, instanceof, int, interface, long, native, new, null,
      * package, private, protected, public, return, short, static, super, switch,
      * synchronized, this, throw, throws, transient, true, try, void, volatile, while
      */
-    COMMENT,
-
-    /* 保留字 */
+    ABSTRACT, BOOLEAN, BREAK, BYTE, CASE, CATCH, CHAR, CLASS, CONST, CONTINUE,
+    DEFAULT, DO, DOUBLE, ELSE, EXTENDS, FALSE, FINAL, FINALLY, FLOAT, FOR, GOTO,
+    IF, IMPLEMENTES, IMPORT, INSTANCEOF, INT, INTERFACE, LONG, NATIVE, NEW, NULL,
+    PACKAGE, PRIVATE, PROTECTED, PUBLIC, RETURN, SHORT, STATIC, SUPER, SWITCH,
+    SYNCHRONIZED, THIS, THROW, THROWS, TRANSIENT, TRUE, TRY, VOID, VOLATILE, WHILE,
+    
+    /**
+     * 赋值运算符号: 0x110
+     * = += -= *= /= %= &=
+     * ^= |= >>= <<= >>>=
+     */
+    ASSIGN, ADD_ASSIGN, MINUS_ASSIGN, MUL_ASSIGN, DIV_ASSIGN, MOD_ASSIGN, AND_ASSIGN,
+    XOR_ASSIGN, OR_ASSIGN, RIGHT_SHIFT_ASSIGN, LEFT_SHIFT_ASSIGN, UNSIGNED_RIGHT_SHIRT_ASSIGN,
     
 } TokenType;
 
