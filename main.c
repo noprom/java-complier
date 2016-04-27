40acc075a4ada57779d2c2d676f5ff4d168c2113
noprom <tyee.noprom@qq.com>
2016-04-27 12:17:37 +0800

    add main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  java_complier
+//
+//  Created by noprom on 4/27/16.
+//  Copyright © 2016 tyee.noprom@qq.com. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
