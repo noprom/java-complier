f356966fddfbc6fb2097e40bffa60f1724f2e2e8
noprom <tyee.noprom@qq.com>
2016-05-01 14:42:51 +0800

    optimize App

--- App.java
+++ App.java
@@ -1,4 +1,4 @@
-//package aspectj.trace.core.app;
+package aspectj.trace.core.app;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -11,16 +11,24 @@ import java.io.IOException;
  * Date: 16/3/4 下午4:38.
  */
 public class App {
-    
-    public void C8() {
-        System.out.println("In C8");
-        for (int i = 0; i < 10; i++) {
-            C10();
-        }
+
+    public void A1() {
+        System.out.println("In A1");
+        A2();
     }
 
-    public void C10() {
-        System.out.println("In C10");
+    public void A2() {
+        System.out.println("In A2");
+    }
+
+    public void A3() {
+        System.out.println("In A3");
+    }
+
+    public void A4() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("In A4");
+        }
     }
 
     public static void main(String[] args) throws IOException {
