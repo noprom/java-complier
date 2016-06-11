e96bd6fb39e6b67aabfd06d0344325253f1aaede
noprom <tyee.noprom@qq.com>
2016-06-11 20:12:57 +0800

    init generator

---
+++ generator.cpp
@@ -0,0 +1,9 @@
+//
+//  generator.cpp
+//  java_complier
+//
+//  Created by noprom on 6/11/16.
+//  Copyright © 2016 tyee.noprom@qq.com. All rights reserved.
+//
+
+#include "generator.h"
---
+++ generator.h
@@ -0,0 +1,14 @@
+//
+//  generator.h
+//  java_complier
+//
+//  Created by noprom on 6/11/16.
+//  Copyright © 2016 tyee.noprom@qq.com. All rights reserved.
+//
+
+#ifndef _GENERATOR_H
+#define _GENERATOR_H
+
+#include <stdio.h>
+
+#endif
